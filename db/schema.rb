4b4caf0905533ff5b7cf573a0a87839365182edb
ijann <sugey90210@gmail.com>
2021-12-02 23:46:26 -0300

    JWT configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_002956) do
+ActiveRecord::Schema.define(version: 2021_12_03_020702) do
 
   create_table "generos", force: :cascade do |t|
     t.string "nombre"
@@ -52,7 +52,9 @@ ActiveRecord::Schema.define(version: 2021_12_03_002956) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
